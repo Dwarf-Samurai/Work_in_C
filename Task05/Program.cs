fc07e4178aa2e992dccf53b9803da3bbd838398e
varangian <officer018@gmail.com>
2023-09-21 21:31:44 +0700

    Добавлена задача №8

---
+++ Task05/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number - number;
+
+while(count < number)
+{ 
+ 
+    count = count + 1;
+ 
+    if (count %2== 0)
+    {
+        Console.Write($"{count} ");
+    }
+}
